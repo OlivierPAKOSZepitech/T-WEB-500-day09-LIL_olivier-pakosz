8c844594979bc3c5a4e1b058b5f47eae360970c7
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-05 12:27:49 +0200

    ajout task02

--- task02.php
+++ task02.php
@@ -1,6 +1,9 @@
 <?php
 
-function render_body($string){
-    if ($string == $home):
-        return 
+function render_body($page){
+    if (file_exists($page . '.html')) {
+        return file_get_contents($page . '.html');
+    } else {
+        return '<p>Unknown page</p>';
+    }
 }
